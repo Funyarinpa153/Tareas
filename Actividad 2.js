9f2a84ceee54cbd58b25f204f554b8fbc8475c51
Diego Espinoza <diego.espinoza@galileo.edu>
2021-02-08 17:56:50 -0600

    Actividad 2

---
+++ Actividad 2.js
@@ -0,0 +1,33 @@
+var mes = 10
+
+
+switch(mes) {
+  case 12:
+  case 1:
+  case 2:
+    alert("¡Es invierno!")
+    break;
+  
+  case 3:
+  case 4:
+  case 5:
+    alert("¡Es primavera!")
+    break;
+    
+  case 6:
+  case 7:
+  case 8:
+    alert("¡Es verano!")
+    break;
+    
+  case 9:
+  case 10:
+  case 11:
+    alert("¡Es otoño!")
+    break;
+    
+}
+
+alert("Director: Espinoza González, Diego Antonio \n 21004332")
+alert("Productor: Espinoza González, Diego Antonio \n 21004332")
+alert("Desarrollador: Espinoza González, Diego Antonio \n 21004332")
